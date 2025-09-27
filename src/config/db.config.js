95c8eecad4a03d99f3ac7311d705cafaf01ace43
Yididya Kedir <kedirjedidiha@gmail.com>
2025-09-27 18:00:05 +0300

    enforced environment variable usage

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'postgres',
-  database: process.env.DB_NAME || 'clothingDB',
-  password: process.env.DB_PASSWORD || '0000',
-  port: process.env.DB_PORT || 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 };
