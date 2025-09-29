12b1d4348d3e4dc218945f523c0f1d72c33713cd
Yididya Kedir <kedirjedidiha@gmail.com>
2025-09-29 15:44:23 +0300

    fix: Add user ID validation and improve error handling in dashboardController and dashboardService

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,14 +1,59 @@
 const product = require("../services/dashboardService");
 
 exports.getTotalSoldData = async (req, res) => {
-    
+    try {
+        const {userId} = req.body;
+        if(!userId){
+            return res.status(400).json({error: "Missing required user ID."});
+        }
+        const data = await product.getTotalSold(userId);
+        res.status(200).json(data);
+
+    } catch (error) {
+        console.error("Error fetching total sold data:", error.message);
+        res.status(500).json({ error: "Failed to fetch total sold data" });
+    }
 }
-exports.getTotalProductData = async (req, res) => {
-    
+exports.getTotalRevenueData = async (req, res) => {
+    try {
+      const { userId } = req.body;
+      if (!userId) {
+        return res.status(400).json({ error: "Missing required user ID." });
+      }
+      const data = await product.getTotalRevenue(userId);
+      res.status(200).json(data);
+    } catch (error) {
+      console.error("Error fetching total revenue data:", error.message);
+      res.status(500).json({ error: "Failed to fetch total revenue data" });
+    }
 }
-exports.getStokeNotification = async (req, res) => {
-    
+exports.getStokeNotificationMessage = async (req, res) => {
+    try {
+      const { userId } = req.body;
+      if (!userId) {
+        return res.status(400).json({ error: "Missing required user ID." });
+      }
+      const data = await product.getStokeNotification(userId);
+      res.status(200).json(data);
+    } catch (error) {
+      console.error("Error fetching stoke notification data:", error.message);
+      res
+        .status(500)
+        .json({ error: "Failed to fetch stoke notification data" });
+    }
 }
 exports.getOrderNotification = async (req, res) => {
-    
+    try {
+      const { userId } = req.body;
+      if (!userId) {
+        return res.status(400).json({ error: "Missing required user ID." });
+      }
+      const data = await product.getOrderNotification(userId);
+      res.status(200).json(data);
+    } catch (error) {
+      console.error("Error fetching stoke notification data:", error.message);
+      res
+        .status(500)
+        .json({ error: "Failed to fetch stoke notification data" });
+    }
 }
--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -1,23 +1,23 @@
 const db = require("../db/pool");
 
 async function getTotalSold(userId) {
-    const sql = `
+  const sql = `
         SELECT SUM(ord_items.quantity) AS total_sold FROM order_items ord_items
-    `
-    const result = await db.query(sql, [userId]);
-    return result.rows
+    `;
+  const result = await db.query(sql, [userId]);
+  return result.rows[0];
 }
 
 async function getTotalRevenue(userId) {
-    const sql = `
+  const sql = `
         SELECT SUM(ord_it.quantity * ord_it.price) AS total_revenue FROM order_items ord_it
     `;
-    const result = await db.query(sql, [userId])
-    return result.rows;
+  const result = await db.query(sql, [userId]);
+  return result.rows[0];
 }
 
 async function getStockNotification(userId) {
-    const sql = `
+  const sql = `
         SELECT 
         prod.product_id, 
         prod.name, 
@@ -27,13 +27,13 @@ async function getStockNotification(userId) {
     LEFT JOIN 
         product_variants prod_var ON prod.product_id = prod_var.product_id
     WHERE prod_var.stock_quantity < 20
-    `
-    const result = await db.query(sql, [userId])
-    return result.rows
+    `;
+  const result = await db.query(sql, [userId]);
+  return result.rows[0];
 }
 
 async function getOrderNotification(userId) {
-    const sql = `
+  const sql = `
         SELECT 
             p.product_id,
             p.name AS product_name,
@@ -44,14 +44,14 @@ async function getOrderNotification(userId) {
         JOIN order_items oi ON o.order_id = oi.order_id
         JOIN product_variants pv ON oi.variant_id = pv.variant_id
         JOIN products p ON pv.product_id = p.product_id;
-    `
-    const result = await db.query(userId);
-    return result.rows
+    `;
+  const result = await db.query(sql, [userId]);
+  return result.rows[0];
 }
 
 module.exports = {
-    getTotalSold,
-    getTotalRevenue,
-    getStockNotification,
-    getOrderNotification
-}
+  getTotalSold,
+  getTotalRevenue,
+  getStockNotification,
+  getOrderNotification,
+};
