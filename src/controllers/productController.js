0265cadf8d58b9d619914ba38a45baa351115a8a
Yididya Kedir <kedirjedidiha@gmail.com>
2025-09-30 14:00:59 +0300

    Fixed Product controller issue

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -59,13 +59,13 @@ exports.getProductByIdController = async (req, res) => {
       return res.status(400).json({ error: "Invalid product ID format." });
     }
 
-    const product = await product.getProductById(productId);
-
-    if (!product) {
+    const productData = await product.getProductById(productId);
+    
+    if (!productData) {
       return res.status(404).json({ error: "Product not found" });
     }
 
-    res.status(200).json(product);
+    res.status(200).json(productData);
   } catch (error) {
     console.error("Error fetching product:", error.message);
     res.status(500).json({ error: "Failed to fetch product" });
