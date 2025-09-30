42399c727b1f2c82283cd313b4fedb7d5af81fbe
Oliyad Diriba <oliyaddiriba75@gmail.com>
2025-09-30 14:39:31 +0300

    stock service added

---
+++ src/services/stockStatusService.js
@@ -0,0 +1,41 @@
+const db = require("../db/pool");
+
+async function getInStock() {
+    const sql = `
+        SELECT 
+            prod.product_id,
+            prod.name,
+            prod_var.stock_quantity AS stock_info
+            prod_var.image_url
+        FROM 
+            products prod
+        LEFT JOIN
+            produt_variants prod_var ON prod.product_id = prod_var.product_id
+        WHERE prod_var.stock_quantity > 0
+    `
+
+    const result = await db.query(sql);
+    return result.row[0];
+}
+
+async function getOutOfStock() {
+    const sql = `
+        SELECT 
+            prod.product_id,
+            prod.name,
+            prod_var.stock_quantity AS stock_info
+            prod_var.image_url
+        FROM 
+            products prod
+        LEFT JOIN
+            produt_variants prod_var ON prod.product_id = prod_var.product_id
+        WHERE prod_var.stock_quantity = 0
+    `;
+    const result = await db.query(sql);
+    return result.rows[0];
+}
+
+module.exports = {
+    getInStock,
+    getOutOfStock
+}
