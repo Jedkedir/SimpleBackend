21955eec56bdf7c8d3483d5079daf51c7a4859c6
Oliyad Diriba <oliyaddiriba75@gmail.com>
2025-09-30 17:27:59 +0300

    query for feature and best seller

--- src/services/homeService.js
+++ src/services/homeService.js
@@ -3,6 +3,21 @@ const db = require("../db/pool");
 
 async function getBestSelling() {
   const sql = `
+      SELECT 
+          p.product_id,
+          pv.variant_id,
+          pv.image_url AS variant_image,
+          pv.price,
+          SUM(oi.quantity) AS total_sold
+      FROM order_items oi
+      JOIN orders o ON oi.order_id = o.order_id
+      JOIN product_variants pv ON oi.variant_id = pv.variant_id
+      JOIN products p ON pv.product_id = p.product_id
+      WHERE o.status = 'Completed'
+      GROUP BY p.product_id, pv.variant_id, pv.image_url, pv.price
+      ORDER BY total_sold DESC
+      LIMIT 10;
+
     `;
   const result = await db.query(sql);
   return result.rows;
@@ -10,6 +25,15 @@ async function getBestSelling() {
 
 async function getFeatures() {
   const sql = `
+    SELECT p.product_id, p.name, p.price, p.base_image_url, c.name AS category_name
+    FROM products p
+    JOIN categories c ON p.category_id = c.category_id
+    WHERE p.product_id IN (
+        SELECT MIN(product_id) 
+        FROM products 
+        GROUP BY category_id
+    )
+    LIMIT 10;
   `;
   const result = await db.query(sql);
   return result.rows;
