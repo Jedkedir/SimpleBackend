cb90b76acb5f681c29d1c36c78ec09d8585c54c8
Oliyad Diriba <oliyaddiriba75@gmail.com>
2025-10-01 13:49:43 +0300

    remove unwanted file

--- backendFile/src/routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const categoryController = require('../control/categoryControl')
-
-router.get('/getCategory', categoryController.getCategory);
-router.post('/addCategory', categoryController.addCategory);
-
-module.exports = router;
--- backendFile/src/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-const productController = require('../control/prodControl');
-
-router.get('/getproducts', productController.getProducts);
-router.post('/addProducts', productController.createProduct);
-
-module.exports = router
